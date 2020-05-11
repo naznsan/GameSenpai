bc8617b2e49877ab354660eb8acb6052242bf8f0
tootboi <danielyim12@gmail.com>
2020-05-11 23:19:09 +1200

    added filter functionality

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -12,7 +12,27 @@ function test(str) {
     console.log(str);
 }
 
+function filter(item) {
+    var coachDiv = document.getElementsByClassName("coach");
+    if (item == "none") {
+        item = "coach";
+    }
+    for (var i = 0; i < coachDiv.length; i++) {
+        if (coachDiv[i].classList.contains(item)) {
+            coachDiv[i].classList.remove("fadeOut");
+            coachDiv[i].classList.remove("hide");
+        } else {
+            coachDiv[i].classList.add("fadeOut");
+            var index = i;
+            setTimeout(function() {
+                coachDiv[index].classList.add("hide");
+            }, 300);
+        }
+    }
+}
+
 module.exports = { 
     functionInHelper,
-    test
+    test,
+    filter
 };
