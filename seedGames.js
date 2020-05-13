ff77f9081c10623e4635d02f758f75f406a3cd00
tootboi <danielyim12@gmail.com>
2020-05-13 12:25:42 +1200

    added images to games

--- seedGames.js
+++ seedGames.js
@@ -6,34 +6,42 @@ games = [
     {
         name: "Sims 4",
         description: "The most hardcore real-life simulator.",
+        image: "https://imgur.com/O3AOhL7",
     },
     {
         name: "Minecraft",
         description: "Hack and Slash your way to the End.",
+        image: "https://imgur.com/dSq78XX",
     },
     {
         name: "Microsoft Windows Pinball",
         description: "Feel the intensity in this global favorite.",
+        image: "https://imgur.com/dgKtVJD",
     },
     {
         name: "Minesweeper",
         description: "Put the matter of life and death in your own hands.",
+        image: "https://imgur.com/eZTjiFR",
     },
     {
         name: "Microsoft Solitaire",
         description: "Klondike is the only real Solitaire.",
+        image: "https://imgur.com/20RfqfB",
     },
     {
         name: "Club Penguin",
         description: "F",
+        image: "https://imgur.com/MGPXiY1",
     },
     {
         name: "Roblox Counter Blox",
         description: "Like CS:GO, but edgier... because it's made of blocks.",
+        image: "https://imgur.com/Ta2GvzF",
     },
     {
         name: "Farming Simulator 2008",
         description: "Feel the rumble of the 300 horsepower engine.",
+        image: "https://imgur.com/7rf14xl",
     },
 ];
 
